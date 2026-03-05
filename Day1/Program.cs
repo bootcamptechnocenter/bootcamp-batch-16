2bcf432639ea9bf484a2d14caa7c76d5e29c7293
Abu Amar <abuamar.albadawi@gmail.com>
2026-03-05 14:02:09 +0700

    init training day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,20 @@
+﻿string namaPerusahaan = "PT. ABC";
+int tahunBerdiri = 1990;
+decimal omzet = 1_000_000_000m;
+bool aktif = true;
+string? catatan = null;
+
+Console.WriteLine($"Nama Perusahaan: {namaPerusahaan}");
+Console.WriteLine($"Tahun Berdiri: {tahunBerdiri}");
+Console.WriteLine($"Omzet: {omzet:C}");
+Console.WriteLine($"Aktif: {aktif}");
+Console.WriteLine($"Catatan: {(catatan ?? "Tidak ada catatan")}");
+
+int a = 10;
+int b = 20;
+
+Console.WriteLine($"Hasil penjumlahan: {a + b}");
+Console.WriteLine($"Hasil pengurangan: {a - b}");
+Console.WriteLine($"Hasil perkalian: {a * b}");
+Console.WriteLine($"Hasil pembagian: {b / a}");
+Console.WriteLine($"Hasil modulus: {b % a}");
