d789e7c3102cddca574089a53ce7fba91322384e
Abu Amar <abuamar.albadawi@gmail.com>
2026-03-06 13:28:56 +0700

    add if else material

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int nilai = 80;
+
+void printNilai(string nilai)
+{
+    Console.WriteLine($"Nilai kamu adalah {nilai}");
+}
+
+// IF ELSE
+
+if (nilai >= 90)
+{
+    printNilai("A");
+}
+else if (nilai >= 80)
+{
+    printNilai("B");
+}
+else if (nilai >= 70)
+{
+    printNilai("C");
+}
+else if (nilai >= 60)
+{
+    printNilai("D");
+}
+else
+{
+    printNilai("E");
+}
+
+string result = nilai >= 90 ? "A" : nilai >= 80 ? "B" : nilai >= 70 ? "C" : nilai >= 60 ? "D" : "E";
+
+printNilai(result);
+
