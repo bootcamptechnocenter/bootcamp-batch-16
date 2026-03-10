f36f7076d0b795681b1e2a16e614af15a20515af
A2852 <suryaajipratama16@gmail.com>
2026-03-10 16:12:16 +0700

    feat: add ApiResponse class and MasterGeneralController

---
+++ IDMS/Controllers/MasterGeneralController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IDMS.Controllers
+{
+    [ApiController]
+    [Route("master/general")]
+    public class MasterGeneralController : Controller
+    {
+        
+    }
+}
--- IDMS/obj/Debug/net10.0/IDMS.AssemblyInfo.cs
+++ IDMS/obj/Debug/net10.0/IDMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IDMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f662d3396d27042126e4d358f4c41103975319b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04149b857740e05ed466cc7f8fec33118e9adcc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("IDMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IDMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
